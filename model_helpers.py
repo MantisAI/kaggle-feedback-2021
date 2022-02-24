ed0235cac80b386316c45f87b8275cdc59142fa3
Daniel Popescu <danielpopescu93@gmail.com>
2022-02-24 11:08:58 +0200

    fix model loading

--- model_helpers.py
+++ model_helpers.py
@@ -1,12 +1,15 @@
 from transformers import AutoTokenizer, AutoConfig, AutoModelForTokenClassification
 
-def load_model(model_name, model_path):
+def load_model(model_name, local_model_path, pretrained_model_path=None):
     tokenizer = AutoTokenizer.from_pretrained(model_name, add_prefix_space=True)
-    tokenizer.save_pretrained(model_path)
+    tokenizer.save_pretrained(local_model_path)
     config_model = AutoConfig.from_pretrained(model_name) 
     config_model.num_labels = 15
-    config_model.save_pretrained(model_path)
+    config_model.save_pretrained(local_model_path)
+    load_model_path = model_name
+    if pretrained_model_path:
+        load_model_path = pretrained_model_path
     model = AutoModelForTokenClassification.from_pretrained(
-                    model_path,config=config_model)
+                    load_model_path,config=config_model)
 
     return model, tokenizer
